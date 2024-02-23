fe8c66da7a8695723b07ae2a2d1fc5ae52868120
Sharooq-Mohammad <152411043+Sharooq-Mohammad@users.noreply.github.com>
2024-02-23 11:14:12 +0530

    added clear function and few more buttons

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ def btn_click(item):
     global expression
     expression = expression + str(item)
     input_text.set(expression)
+
+def bt_equal():
+    global expression
+    result = str(eval(expression))
+    input_text.set(result)
+    expression = ""
+
+def bt_clear(): 
+    global expression 
+    expression = "" 
+    input_text.set("")
   
 expression = ""
 
@@ -21,5 +32,13 @@ input_field.pack(ipady=10)
 
 # btns_frame = Frame(win)
 # btns_frame.pack()
+
+clear = Button(btns_frame, text = "C", fg = "black", width = 32, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
+
+divide = Button(btns_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("/")).grid(row = 0, column = 3, padx = 1, pady = 1)
+seven = Button(btns_frame, text = "7", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(7)).grid(row = 1, column = 0, padx = 1, pady = 1)
+eight = Button(btns_frame, text = "8", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(8)).grid(row = 1, column = 1, padx = 1, pady = 1)
+nine = Button(btns_frame, text = "9", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(9)).grid(row = 1, column = 2, padx = 1, pady = 1)
+equals = Button(btns_frame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
 
 win.mainloop()
